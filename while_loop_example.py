32136fe06314e4ded794f953f0aa57e0be916c47
Yonuel275 <antoniorodriguez5540@gmail.com>
2024-11-07 14:08:47 -0500

    Add files via upload

---
+++ while_loop_example.py
@@ -0,0 +1,18 @@
+# While loop example
+'''
+run_again = input("Enter 'run' to run the program or 'exit' to end: ")
+
+while run_again.lower() != "exit":
+    print("Do something...")
+    run_again = input("Enter 'yes' to run or 'exit' to end: ")
+# Loop breaks here
+print("Thanks for using the problem... goodbye!")
+'''
+# While loop  that runs 5 times
+
+num =  0
+
+while num < 5:
+    print("\U0001f600" * 50)
+    num += 1
+print("Loop has ended!")
